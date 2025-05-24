b3a699c44d618d7210fb82b7bc3a190c2809c967
InGer21 <engelbertrodriguez21@gmail.com>
2025-05-24 02:23:53 -0400

    Modificacion  de vistas

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create(['email'=>'enge@example.com']);
+        User::factory(19)->hasPost(4)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+
+
     }
 }
